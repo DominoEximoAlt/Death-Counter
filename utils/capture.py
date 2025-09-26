9805b5180040b9190101502759dcb46d08c983d6
DominoEximoAlt <DominoEximo231@gmail.com>
2025-09-26 21:58:34 +0200

    Real time screen recording

--- utils/capture.py
+++ utils/capture.py
@@ -1,4 +1,7 @@
+import cv2
 import mss
+import time
+import numpy as np
 import mss.tools
 
 
@@ -15,11 +18,33 @@ with mss.mss() as sct:
         "height": 1000,
         "mon": monitor_number,
     }
-    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+    while "Screen capturing":
+        last_time = time.time()
+
+        # Get raw pixels from the screen, save it to a Numpy array
+        img = np.array(sct.grab(monitor))
+
+        # Display the picture
+        cv2.imshow("OpenCV/Numpy normal", img)
+
+        # Display the picture in grayscale
+        # cv2.imshow('OpenCV/Numpy grayscale',
+        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+        print(f"fps: {1 / (time.time() - last_time)}")
+
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
+
+    ##output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
 
     # Grab the data
-    sct_img = sct.grab(monitor)
+    ##sct_img = sct.grab(monitor)
 
     # Save to the picture file
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-    print(output)
+    ##mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+    ##print(output)
