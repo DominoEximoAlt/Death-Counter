a2fb6bc1202fcc82589b2f6b35570583e0715a92
DominoEximoAlt <DominoEximo231@gmail.com>
2026-01-19 20:39:32 +0100

    updater update p2

--- deathcounter/updater.py
+++ deathcounter/updater.py
@@ -1,12 +1,22 @@
-import sys, time, shutil, subprocess
+import sys, time, shutil, subprocess, os
 
 old_exe = sys.argv[1]
-new_exe = sys.argv[2]
+new_folder = sys.argv[2]
 
 time.sleep(1)  # let main app exit
 
-backup = old_exe + ".bak"
-shutil.move(old_exe, backup)
-shutil.move(new_exe, old_exe)
+# Get the DeathCounter folder path (parent of the exe)
+old_folder = os.path.dirname(old_exe)
 
-subprocess.Popen([old_exe])
+# Backup old folder
+backup = old_folder + ".bak"
+if os.path.exists(backup):
+    shutil.rmtree(backup)
+shutil.move(old_folder, backup)
+
+# Move new folder to replace old location
+shutil.move(new_folder, old_folder)
+
+# Run the updated exe
+new_exe = os.path.join(old_folder, os.path.basename(old_exe))
+subprocess.Popen([new_exe])
